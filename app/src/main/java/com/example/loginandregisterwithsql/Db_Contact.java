6a658f655688992febaaaaa0e8906f3884042359
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-19 03:04:13 +0800

    Feat : add db_contact connect server via java

--- app/src/main/java/com/example/loginandregisterwithsql/Db_Contact.java
+++ app/src/main/java/com/example/loginandregisterwithsql/Db_Contact.java
@@ -1,4 +1,14 @@
 package com.example.loginandregisterwithsql;
 
 public class Db_Contact {
+
+    public static String ip = "192.168.21.1";
+    public static String folderName = "android-api";
+    public static String fileNameRegister = "api-register.php";
+    public static String fileNameLogin = "api-login.php";
+
+
+    public static final String urlRegister = "http://" + ip + "/" + folderName + "/" + fileNameRegister;
+    public static final String urlLogin = "http://" + ip + "/" + folderName + "/" + fileNameLogin;
+
 }
